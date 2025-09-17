f09b260aeabffa34231a5f006287eb8183c398b9
Lahiru Dilshan Rathnayake <rathnay-cs22042@stu.kln.ac.lk>
2025-09-18 00:17:56 +0530

    Update qrscanner.py

--- qrscanner.py
+++ qrscanner.py
@@ -3,7 +3,7 @@ from pyzbar.pyzbar import decode
 from PIL import Image
 
 myqr = qrcode.make('http://www.youtube.com/@LahiruDilshan-ct4mq')
-myqr1 = qrcode.make('https://www.youtube.com/watch?v=y1zJiHcoTA4&list=PLpUo7TyHQvlu0KzY9b-od1y4zKQ6kkPvk&pp=0gcJCaIEOCosWNin ')
+myqr1 = qrcode.make('https://youtu.be/pQw5GGpEmI8?si=w_gGzXWQyPgnlFIj')
 
 myqr.save("myqr.png", scale = 8)
 myqr1.save("myqr1.png", scale = 7)
